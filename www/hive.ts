1e768949a5eda58897a69a14461259705c10a33a
Benjamin Piette <benjamin@findizer.fr>
2019-11-25 09:19:58 +0800

    Converted Hive plugin to TS

---
+++ www/hive.ts
@@ -0,0 +1,357 @@
+/*
+ * Copyright (c) 2019 Elastos Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+var exec = cordova.exec;
+
+class FileImpl implements Hive.File  {
+    objId  = null;
+    plugin = null;
+    clazz  = 4;
+
+    getLastInfo(onSuccess: (info: any)=>void, onError?: (err: string)=>void) {
+        exec(onSuccess, onError, 'HivePlugin', 'getLastInfo', [this.clazz, this.objId]);
+    }
+
+    getInfo(onSuccess: (info: any) => void, onError?: (err: string) => void): Promise<any> {
+        return this.plugin.getPromise(this, 'getInfo', []);
+    }
+
+    moveTo(destPath: string): Promise<any> {
+        return this.plugin.getPromise(this, 'moveTo', [destPath]);
+    }
+
+    copyTo(newPath: string): Promise<any> {
+        return this.plugin.getPromise(this, 'copyTo', [newPath]);
+    }
+
+    deleteItem(): Promise<any> {
+        return this.plugin.getPromise(this, 'deleteItem', []);
+    }
+
+    readData(length: Opaque<number, "Int">): Promise<any> {
+        return this.plugin.getPromise(this, 'readData', [length]);
+    }
+    
+    writeData(data: any): Promise<any> {
+        return this.plugin.getPromise(this, 'writeData', [data]);
+    }
+
+    commit(): Promise<any> {
+        return this.plugin.getPromise(this, 'commitData', []);
+    }
+
+    discard(onSuccess?: () => void) {
+        exec(onSuccess, null, 'HivePlugin', 'discardData', [this.clazz, this.objId]);
+    }
+}
+
+class DirectoryImpl implements Hive.Directory {
+    objId  = null;
+    plugin = null;
+    clazz  = 3;
+
+    getLastInfo(onSuccess: (info: any)=>void, onError?: (err: string)=>void) {
+        exec(onSuccess, onError, 'HivePlugin', 'getLastInfo', [this.clazz, this.objId]);
+    }
+    getInfo(): Promise<any> {
+        return this.plugin.getPromise(this, 'getInfo', []);
+    }
+    createDirectory(name: string): Promise<any> {
+        var plugin = this.plugin;
+        return this.plugin.getPromise(this, 'createDir', [name]).then(
+            function(ret) {
+                var directory = new DirectoryImpl();
+                directory.objId = ret.id;
+                directory.plugin  = plugin;
+                return directory;
+            }
+        );
+    }
+    getDirectory(name: string): Promise<any> {
+        var plugin = this.plugin;
+        return this.plugin.getPromise(this, 'getDir', [name]).then(
+            function(ret) {
+                var directory = new DirectoryImpl();
+                directory.objId = ret.id;
+                directory.plugin  = plugin;
+                return directory;
+            }
+        );
+    }
+    createFile(name: string): Promise<any> {
+        var plugin = this.plugin;
+        return this.plugin.getPromise(this, 'createFile', [name]).then(
+            function(ret) {
+                var file = new FileImpl();
+                file.objId = ret.id;
+                file.plugin = plugin;
+                return file;
+            }
+        );
+    }
+    getFile(name: string): Promise<any> {
+        var plugin = this.plugin;
+        return this.plugin.getPromise(this, 'getFile', [name]).then(
+            function(ret) {
+                var file = new FileImpl();
+                file.objId = ret.id;
+                file.plugin = plugin;
+                return file;
+            }
+        );
+    }
+    getChildren(): Promise<any> {
+        return this.plugin.getPromise(this, 'getChildren', []).then(
+            function(ret) {
+                return ret.items;
+            }
+        );
+    }
+    moveTo(destPath: string): Promise<any> {
+        return this.plugin.getPromise(this, 'moveTo', [destPath]);
+    }
+    copyTo(newPath: string): Promise<any> {
+        return this.plugin.getPromise(this, 'copyTo', [newPath]);
+    }
+    deleteItem(): Promise<any> {
+        return this.plugin.getPromise(this, 'deleteItem', []);
+    }
+}
+class DriveImpl implements Hive.Drive {
+    objId  = null;
+    plugin = null;
+    clazz  = 2;
+
+    getLastInfo(onSuccess: (info: any) => void, onError?: (err: string) => void) {
+        exec(onSuccess, onError, 'HivePlugin', 'getLastInfo', [this.clazz, this.objId]);
+    }
+    getInfo(): Promise<any> {
+        return this.plugin.getPromise(this, 'getInfo', []);
+    }
+    rootDirectory(): Promise<any> {
+        var plugin = this.plugin;
+        return this.plugin.getPromise(this, 'rootDir', []).then(
+            function(ret) {
+                var directory = new DirectoryImpl();
+                directory.objId = ret.id;
+                directory.plugin  = plugin;
+                return directory;
+            }
+        );
+    }
+    createDirectory(path: string): Promise<any> {
+        var plugin = this.plugin;
+        return this.plugin.getPromise(this, 'createDir', [path]).then(
+            function(ret) {
+                var directory = new DirectoryImpl();
+                directory.objId = ret.id;
+                directory.plugin  = plugin;
+                return directory;
+            }
+        );
+    }
+    getDirectory(path: string): Promise<any> {
+        var plugin = this.plugin;
+        return this.plugin.getPromise(this, 'getDir', [path]).then(
+            function(ret) {
+                var directory = new DirectoryImpl();
+                directory.objId = ret.id;
+                directory.plugin  = plugin;
+                return directory;
+            }
+        );
+    }
+    createFile(path: string): Promise<any> {
+        var plugin = this.plugin;
+        return this.plugin.getPromise(this, 'createFile', [path]).then(
+            function(ret) {
+                var file = new FileImpl();
+                file.objId = ret.id;
+                file.plugin = plugin;
+                return file;
+            }
+        );
+    }
+    getFile(path: string): Promise<any> {
+        var plugin = this.plugin;
+        return this.plugin.getPromise(this, 'getFile', [path]).then(
+            function(ret) {
+                var file = new FileImpl();
+                file.objId = ret.id;
+                file.plugin = plugin;
+                return file;
+            }
+        );
+    }
+    getItemInfo(path: string): Promise<any> {
+        return this.plugin.getPromise(this, 'getItemInfo', [path]);
+    }
+}
+
+class ClientImpl implements Hive.Client {
+    objId  = null;
+    plugin = null;
+    clazz  = 1;
+
+    login(handler: Function, onSuccess?: () => void, onError?: (err: string) => void) {
+        var handlerId = this.plugin.addLoginRequestCb(handler);
+        exec(onSuccess, onError, 'HivePlugin', 'login', [this.clazz, this.objId, handlerId]);
+    }
+    logout(onSuccess?: () => void, onError?: (err: string) => void) {
+        exec(onSuccess, onError, 'HivePlugin', 'logout', [this.clazz, this.objId]);
+    }
+    getLastInfo(onSuccess?: (info: any) => void, onError?: (err: string) => void) {
+        exec(onSuccess, onError, 'HivePlugin', 'getLastInfo', [this.clazz, this.objId]);
+    }
+    getInfo(): Promise<any> {
+        return this.plugin.getPromise(this, 'getInfo', []);
+    }
+    getDefDrive(): Promise<any> {
+        var plugin = this.plugin;
+        return this.plugin.getPromise(this, 'getDefDrive', []).then(
+            function (ret) {
+                var drive = new DriveImpl();
+                drive.objId = ret.id;
+                drive.plugin = plugin;
+                return drive;
+            }
+        );
+    }
+}
+
+const LISTENER_LOGIN  = 1;
+const LISTENER_RESULT = 2;
+
+type HivePluginEvent = {
+    callback: Function;
+    object: any;
+};
+
+class HivePluginImpl implements Hive.HivePlugin {
+    clients = [];
+
+    resultIndex = 0;
+    resultEvent: HivePluginEvent[] = [];
+    loginCount  = 0;
+    loginRequest = [];
+    loginEvent: HivePluginEvent;
+
+    constructor() {
+        Object.freeze(HivePluginImpl.prototype);
+        Object.freeze(ClientImpl.prototype);
+        Object.freeze(DriveImpl.prototype);
+        Object.freeze(DirectoryImpl.prototype);
+        Object.freeze(FileImpl.prototype);
+
+        this.setListener(LISTENER_LOGIN,  this.onLoginRequest);
+        this.setListener(LISTENER_RESULT, this.onResultEvent);    
+    
+        exec(function () {}, null, 'HivePlugin', 'initVal', []);
+    }
+
+    addLoginRequestCb(callback) {
+        var eventcb: HivePluginEvent;
+        eventcb.callback = callback;
+
+        this.loginEvent = eventcb;
+        return 0;
+    }
+
+    onLoginRequest(event) {
+        var id = event.id;
+        if (id == 0) {
+            this.loginEvent.callback(event.url);
+        }
+    }
+
+    addResultEventCb = function(callback, object) {
+        var eventcb: HivePluginEvent;
+        eventcb.callback = callback;
+        eventcb.object = object;
+
+        this.resultIndex++;
+        this.resultEvent[this.resultIndex] = eventcb;
+        return this.resultIndex;
+    }
+
+    onResultEvent(event) {
+        var id = event.hid;
+        event.hid = null;
+
+        if (this.resultEvent[id].callback)  {
+            this.resultEvent[id].callback(event);
+        }
+    }
+
+    getPromise(object, method, args) {
+        var me = this;
+        return new Promise(function(resolve, reject) {
+            var onResult = function(ret) {
+                if (null == ret.error)
+                    resolve(ret);
+                else
+                    reject(ret.error);
+            };
+
+            var _args = [
+                object.clazz,
+                object.objId,
+                me.addResultEventCb(onResult, object),
+            ];
+
+            exec(null, null, 'HivePlugin', method, _args.concat(args));
+        });
+    }
+
+    getVersion(onSuccess?: ()=>void, onError?: (err: string)=>void) {
+        exec(onSuccess, onError, 'HivePlugin', 'getVersion', [])
+    }
+
+    setListener(type: any, eventCallback: Function) {
+        exec(eventCallback, null, 'HivePlugin', 'setListener', [type]);
+    }
+    createClient(options: any, onSuccess: (client: ClientImpl) => void, onError?: (err: string) => void) {
+        var client = new ClientImpl();
+        var me = this;
+
+        var _onSuccess = function(ret) {
+            client.objId = ret.id;
+            client.plugin = me;
+            me.clients[client.objId] = client;
+            if (onSuccess)
+                onSuccess(client);
+        }
+
+        if (typeof (options) == "undefined" || options == null ||
+            typeof (options.driveType) != "string") {
+            if (onError) {
+                onError("invalid options");
+            }
+            return;
+        }
+
+        var configStr = JSON.stringify(options);
+        exec(_onSuccess, onError, 'HivePlugin', 'createClient', ["im", configStr]);
+    }
+}
+
+export = new HivePluginImpl();
---
+++ www/types.d.ts
@@ -0,0 +1,374 @@
+/*
+ * Copyright (c) 2019 Elastos Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+type Opaque<T, K> = T & { __opaque__: K };
+type Int = Opaque<number, 'Int'>;
+
+// TODO: Replace with accurate types everywhere there is "any" or "Function".
+
+declare module Hive {
+    /**
+     * The class representing File.
+     * @class
+     */
+    interface File {
+        /**
+         * Get the information(ID, name size, type) of the file got last time.
+         * 
+         * @param {Function} onSuccess  The function to call on success.
+         * @param {Function} onError    The function to call on error.
+         * @return
+         * onSuccess will be called on success, otherwise onError will be called.
+         */
+        getLastInfo(onSuccess: (info: any)=>void, onError?: (err: string)=>void);
+
+        /**
+         * Get the information(ID, name, size, type) of the file from the server.
+         * 
+         * @return
+         * A promise object that contains the information(ID, name, size, type) of the file
+         * will be returned on success, otherwise a promise object that contains error
+         * information will be returned.
+         */
+        getInfo(onSuccess: (info: any)=>void, onError?: (err: string)=>void): Promise<any>;
+
+        /**
+         * Move to a new path.
+         * 
+         * @param {string} destPath     The new path.
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise object that contains error information will be returned.
+         */
+        moveTo(destPath: string): Promise<any>;
+
+        /**
+         * Copy to a new path.
+         * 
+         * @param {string} newPath      The new path.
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise object that contains error information will be returned.
+         */
+        copyTo(newPath: string): Promise<any>;
+
+        /**
+         * Delete.
+         * 
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise object that contains error information will be returned.
+         */
+        deleteItem(): Promise<any>;
+
+        /**
+         * Read data of a specified length sequentially.
+         * 
+         * @param {number} length      The length of data to write.
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise object that contains error information will be returned.
+         */
+        readData(length: Int): Promise<any>;
+
+        /**
+         * Write local change on File.
+         * 
+         * @param {string} data      The data to write.
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise object that contains error information will be returned.
+         */
+        writeData(data: any): Promise<any>;
+
+        /**
+         * Commit local change on File to backend.
+         * 
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise object that contains error information will be returned.
+         */
+        commit(): Promise<any>;
+
+        /**
+         * Discard local change on File.
+         * 
+         * @param {Function} onSuccess  The function to call on success.
+         * @return
+         * onSuccess will be called on success.
+         */
+        discard(onSuccess?: ()=>void);
+    }
+
+    /**
+     * The class representing Directory.
+     * @class
+     */
+    interface Directory {
+        /**
+         * Get the information(ID, name, childCount) of the directory got last time.
+         * 
+         * @param {Function} onSuccess  The function to call on success.
+         * @param {Function} onError    The function to call on error.
+         * @return
+         * onSuccess will be called on success, otherwise onError will be called.
+         */
+        getLastInfo(onSuccess: (info: any)=>void, onError?: (err: string)=>void);
+
+        /**
+         * Get the information(ID, name, childCount) of the directory from the server.
+         * 
+         * @return
+         * A promise object that contains the information(ID, name, childCount) of the file
+         * will be returned on success, otherwise a promise object that contains error
+         * information will be returned.
+         */
+        getInfo(): Promise<any>;
+
+        /**
+         * Create directory with name.
+         * @param {string} name      The directory name.
+         * @return
+         * A directory will be returned on success, otherwise a promise object that contains
+         * error information will be returned.
+         */
+        createDirectory(name: string): Promise<any>;
+
+        /**
+         * Get the directory with a specified name.
+         * 
+         * @param {string} name      The directory name.
+         * @return
+         * A directory will be returned on success, otherwise a promise object that contains
+         * error information will be returned.
+         */
+        getDirectory(name: string): Promise<any>;
+
+        /**
+         * Create file with name.
+         * 
+         * @param {string} name      The file name.
+         * @return
+         * A file will be returned on success, otherwise a promise object that contains
+         * error information will be returned.
+         */
+        createFile(name: string): Promise<any>;
+
+        /**
+         * Get the File with a specified name.
+         * 
+         * @param {string} name      The file name.
+         * @return
+         * A file will be returned on success, otherwise a promise object that contains
+         * error information will be returned.
+         */
+        getFile(name: string): Promise<any>;
+
+        /**
+         * Get children for current directory.
+         * 
+         * @return
+         * The children for current directory will be returned on success, otherwise
+         * a promise object that contains error information will be returned.
+         */
+        getChildren(): Promise<any>;
+
+        /**
+         * Move to a new path.
+         * 
+         * @param {string} destPath     The destination path.
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise object that contains error information will be returned.
+         */
+        moveTo(destPath: string): Promise<any>;
+
+        /**
+         * Copy to a new path.
+         * 
+         * @param {string} newPath      The new path.
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise object that contains error information will be returned.
+         */
+        copyTo(newPath: string): Promise<any>;
+
+        /**
+         * Delete.
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise object that contains error information will be returned.
+         */
+        deleteItem(): Promise<any>;
+    }
+
+    /**
+     * The class representing Drive.
+     * @class
+     */
+    interface Drive {
+        /**
+         * Get the information(ID) of the drive got last time.
+         * 
+         * @param {Function} onSuccess  The function to call on success.
+         * @param {Function} onError    The function to call on error.
+         * @return
+         * onSuccess will be called on success, otherwise onError will be called.
+         */
+        getLastInfo(onSuccess: (info: any)=>void, onError?: (err: string)=>void);
+
+        /**
+         * Get the information(ID) of the drive from the server.
+         * 
+         * @return
+         * A promise object that contains the information(ID) of the file
+         * will be returned on success, otherwise a promise that contains error information
+         * will be returned.
+         */
+        getInfo(): Promise<any>;
+
+        /**
+         * Get the root directory.
+         * 
+         * @return
+         * A directory will be returned on success, otherwise a promise that contains
+         * error information will be returned.
+         */
+        rootDirectory(): Promise<any>;
+
+        /**
+         * Create directory with path.
+         * 
+         * @param {string} path      The directory path.
+         * @return
+         * A directory will be returned on success, otherwise a promise that contains
+         * error information will be returned.
+         */
+        createDirectory(path: string): Promise<any>;
+
+        /**
+         * Get the directory with a specified path.
+         * 
+         * @param {string} path      The directory path.
+         * @return
+         * A directory will be returned on success, otherwise a promise that contains
+         * error information will be returned.
+         */
+        getDirectory(path: string): Promise<any>;
+
+        /**
+         * Create file with path.
+         * 
+         * @param {string} path      The file path.
+         * @return
+         * A file will be returned on success, otherwise a promise that contains
+         * error information will be returned.
+         */
+        createFile(path: string): Promise<any>;
+
+        /**
+         * Get the File with a specified path.
+         * 
+         * @param {string} path      The file path.
+         * @return
+         * A file will be returned on success, otherwise a promise that contains
+         * error information will be returned.
+         */
+        getFile(path: string): Promise<any>;
+
+        /**
+         * Get the information(ID, name, size, type) of the drive with a specified path.
+         * 
+         * @param {string} path      The drive path.
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise that contains error information will be returned.
+         */
+        getItemInfo(path: string): Promise<any>;
+    }
+
+    /**
+     * The class representing Client.
+     * @class
+     */
+    interface Client {
+        /**
+         * Associate a user with the Client.
+         * 
+         * @param {Function} onSuccess  The function to call on success.
+         * @param {Function} onError    The function to call on error.
+         * @param {Function} handler    The function to call.
+         * @return
+         * onSuccess will be called on success, otherwise onError will be called.
+         */
+        login(handler: Function, onSuccess?: ()=>void, onError?: (err: string)=>void);
+
+        /**
+         * Dissociate the user from the Client.
+         * 
+         * @param {Function} onSuccess  The function to call on success.
+         * @param {Function} onError    The function to call on error.
+         * @return
+         * onSuccess will be called on success, otherwise onError will be called.
+         */
+        logout(onSuccess?: ()=>void, onError?: (err: string)=>void);
+
+        /**
+         * Get the last associated user's information with client information.
+         * 
+         * @param {Function} onSuccess  The function to call on success.
+         * @param {Function} onError    The function to call on error.
+         * @return
+         * onSuccess will be called on success, otherwise onError will be called.
+         */
+        getLastInfo(onSuccess?: (info: any)=>void, onError?: (err: string)=>void);
+
+        /**
+         * Get associated user's information with client information.
+         * 
+         * @return
+         * A promise object that contains success information will be returned on success,
+         * otherwise a promise object that contains error information will be returned.
+         */
+        getInfo(): Promise<any>;
+
+        /**
+         * Get the current backend's Drive instance associated with the client's drive.
+         * @return
+         * A drive will be returned on success, otherwise a promise object that contains
+         * error information will be returned.
+         */
+        getDefDrive(): Promise<any>;
+    }
+
+    type driveType = {
+        NATIVE: 1,
+        ONEDRIVE: 2,
+        IPFS: 3
+    };
+
+    interface HivePlugin {
+        getVersion(onSuccess?: ()=>void, onError?: (err: string)=>void);
+        setListener(type: any, eventCallback: Function);
+        createClient(options: any, onSuccess: (client: Client)=>void, onError?: (err: string)=>void);
+    }
+}
